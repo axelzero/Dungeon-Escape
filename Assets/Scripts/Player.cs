553536a96dd13fddfdc892f89d0ac12e47b7ae92
Vitalii <axelzero31@gmail.com>
2019-12-07 19:51:20 +0200

    Added script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class Player : MonoBehaviour
+    {
+         private Rigidbody2D _rb2D;
+
+        private void Start()
+        {
+            _rb2D = gameObject.GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            _rb2D.velocity = new Vector2(horizontalInput, _rb2D.velocity.y);
+        }
+    }
+}
